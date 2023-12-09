d21650ab36d19004328c319a4810c51c1b84d59c
sangchu <didi1484@gmail.com>
2023-12-09 17:06:39 +0900

    feat: 뼈대 클래스 생성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,15 @@
 package bridge;
 
+import bridge.controller.Controller;
+import bridge.domain.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Controller controller = new Controller(new BridgeGame(), new InputView(), new OutputView());
+        controller.start();
     }
 }
---
+++ src/main/java/bridge/controller/Controller.java
@@ -0,0 +1,21 @@
+package bridge.controller;
+
+import bridge.domain.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class Controller {
+    private final BridgeGame bridgeGame;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public Controller(BridgeGame bridgeGame, InputView inputView, OutputView outputView) {
+        this.bridgeGame = bridgeGame;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start() {
+
+    }
+}

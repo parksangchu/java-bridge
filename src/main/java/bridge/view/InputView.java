a846d945b828c925c9cb4278adf494f9f228f272
sangchu <didi1484@gmail.com>
2023-12-09 17:18:14 +0900

    feat: 다리 길이 입력 메서드 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,15 +1,27 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private static final String INT_REGEX = "^[0-9]+$";
+    private static final String TYPE_ERROR = "[ERROR] 다리 길이는 숫자만 입력할 수 있습니다.";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String input = Console.readLine();
+        validateType(input);
+        return Integer.parseInt(input);
+    }
+
+    private void validateType(String input) {
+        if (!input.matches(INT_REGEX)) {
+            throw new IllegalArgumentException(TYPE_ERROR);
+        }
     }
 
     /**

46d47b89f4b03cc66c4044a245c74d1b62444267
sangchu <didi1484@gmail.com>
2023-12-10 14:07:29 +0900

    feat: 움직임 검증 기능 추가

--- src/main/java/bridge/domain/Moving.java
+++ src/main/java/bridge/domain/Moving.java
@@ -1,4 +1,19 @@
 package bridge.domain;
 
 public class Moving {
+    private static final String UPPER_SIDE = "U";
+    private static final String DOWN_SIDE = "D";
+    private final String direction;
+
+    public Moving(String direction) {
+        validate(direction);
+        this.direction = direction;
+    }
+
+    private void validate(String direction) {
+        if (direction.equals(UPPER_SIDE) && direction.equals(DOWN_SIDE)) {
+            throw new IllegalArgumentException("[ERROR] 올바른 방향이 아닙니다.");
+        }
+    }
+
 }

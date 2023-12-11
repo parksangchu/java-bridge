7dd78e9bbdf50b9f2e1cad5a2498c178cdd89534
sangchu <didi1484@gmail.com>
2023-12-11 12:20:43 +0900

    test: Moving 방향 확인 기능 테스트

--- src/test/java/bridge/domain/player/MovingTest.java
+++ src/test/java/bridge/domain/player/MovingTest.java
@@ -1,9 +1,11 @@
 package bridge.domain.player;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -16,5 +18,12 @@ class MovingTest {
                 .hasMessageContaining("[ERROR]");
         assertThatCode(() -> new Moving("U")).doesNotThrowAnyException();
         assertThatCode(() -> new Moving("D")).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("U를 입력하면 위칸이고 D을 입력하면 아래칸이다.")
+    void name() {
+        assertThat(new Moving("U").isUpperSide()).isTrue();
+        assertThat(new Moving("D").isUpperSide()).isFalse();
     }
 }
